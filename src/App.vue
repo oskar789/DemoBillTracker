d1372f831283facd7586c138fbd5123e5b6fe491
oskar789 <oschagollan@gmail.com>
2021-07-19 03:07:02 -0400

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,108 @@
+<template>
+  <main>
+    <AddCategory v-if="shouldShowAddCategory" v-on:addCategory="addCategory"/>
+    <div v-else>
+      <AddBill v-if="shouldShowAddBill" :categories=categories v-on:addBill="addBill"/>
+      <div v-else>
+        <NavBar :categories=categories :activeCategory=activeCategory  v-on:clearActiveCategory=clearActiveCategory v-on:setActiveCategory=setActiveCategory v-on:triggerShowAddCategory=triggerShowAddCategory />
+        <div class="container flex">
+          <div class="w-1/2 bg-grey-lighter">
+            <BillsTable :bills="activeBills" v-on:triggerShowAddBill="triggerShowAddBill" v-on:removeBill="removeBill" />
+          </div>
+          <div class="w-1/2 bg-grey-light pt-4 pl-4 text-2xl">
+            <Chart :bills="activeBills" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+import Vue from 'vue'
+import AddCategory from './components/AddCategory.vue'
+import AddBill from './components/AddBill.vue'
+import NavBar from './components/NavBar.vue'
+import Chart from './components/Chart.vue'
+import BillsTable from './components/BillsTable.vue'
+
+Vue.use(require('vue-moment'))
+
+export default {
+  name: 'app',
+  components: {
+    AddCategory,
+    AddBill,
+    Chart,
+    BillsTable,
+    NavBar
+  },
+  data() {
+    return {
+      bills: [],
+      categories: [],
+      shouldShowAddCategory: false,
+      shouldShowAddBill: false,
+      activeCategory: ''
+    }
+  },
+  methods: {
+    addCategory(category) {
+      this.categories.push(category)
+      this.shouldShowAddCategory = false
+    },
+    addBill(bill) {
+      this.bills.push(bill)
+      this.shouldShowAddBill = false
+    },
+    triggerShowAddCategory() {
+      this.shouldShowAddCategory = true
+    },
+    triggerShowAddBill() {
+      this.shouldShowAddBill = true
+    },
+    removeBill(index) {
+      this.bills = this.bills
+        .slice(0, index)
+        .concat(this.bills.slice(index + 1, this.bills.length))
+    },
+    clearActiveCategory() {
+      this.activeCategory = ''
+    },
+    setActiveCategory(category) {
+      this.activeCategory = category
+    }
+  },
+  watch: {
+    bills() {
+      localStorage.setItem('bills', JSON.stringify(this.bills))
+    },
+    categories() {
+      localStorage.setItem('categories', JSON.stringify(this.categories))
+    }
+  },
+  mounted() {
+    if (localStorage.getItem('bills')) {
+      this.bills = JSON.parse(localStorage.getItem('bills'))
+    }
+
+    if (localStorage.getItem('categories')) {
+      this.categories = JSON.parse(localStorage.getItem('categories'))
+    }
+
+    if (!this.bills.length && !this.categories.length) {
+      this.shouldShowAddCategory = true
+    }
+  },
+  computed: {
+    activeBills() {
+      return this.bills
+        .filter(
+          bill =>
+            this.activeCategory ? bill.category === this.activeCategory : true
+        )
+        .sort((a, b) => (new Date(a.date) < new Date(b.date) ? 1 : -1))
+    }
+  }
+}
+</script>
---
+++ src/components/BillsTable.vue
@@ -0,0 +1,38 @@
+<template>
+  <table class="table w-full">
+    <thead class="bg-blue text-white">
+      <tr>
+        <th class="p-4" scope="col">Date</th>
+        <th class="p-4" scope="col">Amount</th>
+        <th class="p-4" scope="col">Category</th>
+        <th class="p-4" scope="col"></th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr class="p-4 bg-blue-lighter text-center">
+        <td colspan="4" class="p-4" ><button class="underline" @click="triggerShowAddBill">Add new</button></td>
+      </tr>
+      <tr v-for="(bill, index) in bills" :key="index" class="p-4">
+        <td class="p-4">{{bill.date | moment("MMM D YYYY")}}</td>
+        <td class="p-4">${{bill.amount}}</td>
+        <td class="p-4">{{bill.category}}</td>
+        <td class="p-4"><button @click="removeBill(index)">𝗫</button></td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: 'BillsTable',
+  props: ['bills'],
+  methods: {
+    triggerShowAddBill: function() {
+      this.$emit('triggerShowAddBill')
+    },
+    removeBill: function(index) {
+      this.$emit('removeBill', index)
+    }
+  }
+}
+</script>
